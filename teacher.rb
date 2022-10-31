154ff869662a05455bfeca540c4778324c8a2631
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-01 02:30:57 +0300

    Add can_use_services? method

--- teacher.rb
+++ teacher.rb
@@ -5,5 +5,9 @@ class Teacher < person
         super(age, name, parent_permission)
         @specialization = specialization
       end
+
+      def can_use_services?
+        true
+      end
     
 end
