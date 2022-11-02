ef180065017014f97882506ca313c5fbf9640678
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-03 00:11:32 +0300

    Add add_rental method

--- book.rb
+++ book.rb
@@ -9,4 +9,9 @@ class Book
         @author = author
         @rentals = []
       end
+
+      def add_rental(date, person)
+        rental = Rental.new(date, person, self)
+        @rentals << rental unless rentals.includes?(rental)
+      end
 end
