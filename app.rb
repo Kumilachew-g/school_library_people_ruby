bebcd8051fec28dd2b96a9680657e3e17c7e1b0f
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-04 15:46:47 +0300

    Fix linter error

--- app.rb
+++ app.rb
@@ -5,55 +5,55 @@ require_relative './book'
 require_relative './rental'
 
 class App
-    attr_accessor :persons, :books, :rentals
+  attr_accessor :persons, :books, :rentals
 
-    def initialize
-        @books = []
-        @rentals = []
-        @persons = []
-        @classroom = Classroom.new('Class A')
+  def initialize
+    @books = []
+    @rentals = []
+    @persons = []
+    @classroom = Classroom.new('Class A')
+  end
+
+  def create_teacher(specialization, age, name)
+    teacher = Teacher.new(specialization, age, name)
+    @persons << teacher
+    puts 'Teacher created successfuly'
+  end
+
+  def create_student(age, name, parent_permission)
+    student = Student.new(@classroom, age, name, parent_permission: parent_permission)
+    @persons << student
+    puts 'Student created successfuly'
+  end
+
+  def create_rental(date, person, book)
+    rental = Rental.new(date, person, book)
+    @rentals << rental
+    puts 'Rental Created Successfuly'
+  end
+
+  def create_book(title, author)
+    book = Book.new(title, author)
+    @books << book
+    puts 'Book Created Successfuly'
+  end
+
+  def list_of_persons
+    puts 'No person list found' if @persons.length.zero?
+    @persons.each do |person|
+      puts "[#{person.class}] Name: #{person.name}, Age: #{person.age}, Id: #{person.id} "
     end
 
-    def create_teacher(specialization, age, name)
-        teacher = Teacher.new(specialization, age, name)
-        @persons << teacher
-        puts 'Teacher created successfuly'
-      end
-
-      def create_student(age, name, parent_permission)
-        student = Student.new(@classroom, age, name, parent_permission: parent_permission)
-        @persons << student
-        puts 'Student created successfuly'
-      end
-
-      def create_rental(date, person, book)
-        rental = Rental.new(date, person, book)
-        @rentals << rental
-        puts 'Rental Created Successfuly'
-      end
-
-      def create_book(title, author)
-        book = Book.new(title, author)
-        @books << book
-        puts 'Book Created Successfuly'
-      end
-
-      def list_of_persons
-        puts 'No person list found' if @persons.length.zero?
-        @persons.each do |person|
-          puts "[#{person.class}] Name: #{person.name}, Age: #{person.age}, Id: #{person.id} "
-     end
-
-     def list_of_books
-        puts 'No books list found!' if @books.length.zero?
-        @books.each { |book| puts "Book Title: \"#{book.title}\", Book Author: #{book.author}" }
-      end
+    def list_of_books
+      puts 'No books list found!' if @books.length.zero?
+      @books.each { |book| puts "Book Title: \"#{book.title}\", Book Author: #{book.author}" }
     end
+  end
 
-    def display_rentals_by_person_id(person_id)
-        person_rentals = @rentals.select { |rental| rental.person.id == person_id }
-        person_rentals.each do |rental|
-          puts " Book: #{rental.book.title}, Writen By: #{rental.book.author}, Date: #{rental.date}"
-        end
+  def display_rentals_by_person_id(person_id)
+    person_rentals = @rentals.select { |rental| rental.person.id == person_id }
+    person_rentals.each do |rental|
+      puts " Book: #{rental.book.title}, Writen By: #{rental.book.author}, Date: #{rental.date}"
     end
+  end
 end
--- main.rb
+++ main.rb
@@ -1,66 +1,66 @@
 require './app'
 
 class Main
-    def display_options
-        puts ' '
-        puts 'Please choose an option by entering a number:'
-        puts '1 - List all books'
-        puts '2 - List all people'
-        puts '3 - Create a person'
-        puts '4 - Create a book'
-        puts '5 - Create a rental'
-        puts '6 - List all rentals for a given person id'
-        puts '7 - Exit'
-      end
+  def display_options
+    puts ' '
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
 
-      def add_book(app)
-        puts 'Title: '
-        title = gets.chomp
-        puts 'Author: '
-        author = gets.chomp
-        app.create_new_book(title, author)
-      end
+  def add_book(app)
+    puts 'Title: '
+    title = gets.chomp
+    puts 'Author: '
+    author = gets.chomp
+    app.create_new_book(title, author)
+  end
 
-      def add_person(app)
-        puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
-        type = gets.chomp.to_i
-        puts 'Name: '
-        name = gets.chomp
-        puts 'Age: '
-        age = gets.chomp.to_i
-        if type == 1
-          puts 'Has parent permission? [Y/N]: '
-          parent_permission = gets.chomp
-          app.create_new_student(age, name, parent_permission: parent_permission.downcase == 'y')
-        else
-          puts 'Specialization: '
-          specialization = gets.chomp
-          app.create_new_teacher(specialization, age, name)
-        end
-        puts 'You choose invalid menu, Return to menu'
-      end
+  def add_person(app)
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    type = gets.chomp.to_i
+    puts 'Name: '
+    name = gets.chomp
+    puts 'Age: '
+    age = gets.chomp.to_i
+    if type == 1
+      puts 'Has parent permission? [Y/N]: '
+      parent_permission = gets.chomp
+      app.create_new_student(age, name, parent_permission: parent_permission.downcase == 'y')
+    else
+      puts 'Specialization: '
+      specialization = gets.chomp
+      app.create_new_teacher(specialization, age, name)
+    end
+    puts 'You choose invalid menu, Return to menu'
+  end
 
-      def list_person(app)
-        app.list_persons
-      end
+  def list_person(app)
+    app.list_persons
+  end
 
-      def list_books(app)
-        app.list_books
-      end
+  def list_books(app)
+    app.list_books
+  end
 
-      def add_rental(app)
-        puts 'Books is empity ' if app.books.length.zero?
-        puts 'Person is empity' if app.list_persons.length.zero?
-        puts 'Select a book from the following list by number: '
-    
-        app.list_books.each_with_index do |book, index|
-          puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
-        end
+  def add_rental(app)
+    puts 'Books is empity ' if app.books.length.zero?
+    puts 'Person is empity' if app.list_persons.length.zero?
+    puts 'Select a book from the following list by number: '
 
-        book_index = gets.chomp.to_i
-        book = app.books[book_index]
+    app.list_books.each_with_index do |book, index|
+      puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
+    end
 
-        puts 'Select person from the following list by number:'
+    book_index = gets.chomp.to_i
+    book = app.books[book_index]
+
+    puts 'Select person from the following list by number:'
     app.list_persons.each_with_index do |person, index|
       puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
