879883deb84b5258e28d5d6bdf021c152a594e1c
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-04 15:04:01 +0300

    Add add_person method

--- main.rb
+++ main.rb
@@ -20,4 +20,24 @@ class Main
         author = gets.chomp
         app.create_new_book(title, author)
       end
+
+      def add_person(app)
+        puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+        type = gets.chomp.to_i
+        puts 'Name: '
+        name = gets.chomp
+        puts 'Age: '
+        age = gets.chomp.to_i
+        if type == 1
+          puts 'Has parent permission? [Y/N]: '
+          parent_permission = gets.chomp
+          app.create_new_student(age, name, parent_permission: parent_permission.downcase == 'y')
+        else
+          puts 'Specialization: '
+          specialization = gets.chomp
+          app.create_new_teacher(specialization, age, name)
+        end
+    
+        puts 'You choose invalid menu, Return to menu'
+      end
 end
