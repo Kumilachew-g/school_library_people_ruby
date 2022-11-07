8c31f64f6d60e807fd6097b6d072a704d5709b8b
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-07 21:32:29 +0300

    Separate display_options method from the main.rb

--- main.rb
+++ main.rb
@@ -1,17 +1,6 @@
 require './app'
 
 class Main
-  def display_options
-    puts ' '
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-  end
 
   def add_person(app)
     puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
