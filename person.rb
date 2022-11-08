cff85cb3470fc19f7d8ac942c2261dc731f7823a
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-08 20:21:55 +0300

    Update person.rb to implement preserve data

--- person.rb
+++ person.rb
@@ -5,9 +5,9 @@ class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id, :rentals
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(id:, age:, name: 'Unknown', parent_permission: true)
     super()
-    @id = Random.rand(1..1000)
+    @id = id || Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
