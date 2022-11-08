1d7c09be8aded9722e0fd9ef078a5948bfc08d5a
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-08 12:06:30 +0300

    Separate menu method to implement SOLID

--- main.rb
+++ main.rb
@@ -65,22 +65,6 @@ class Main
     app.display_rentals_by_person_id(id)
   end
 
-  def menu(option, app)
-    case option
-    when 1
-      list_books(app)
-    when 2
-      list_person(app)
-    when 3
-      add_person(app)
-    when 4
-      add_book(app)
-    when 5
-      add_rental(app)
-    when 6
-      display_rentals_by_id(app)
-    end
-  end
 
   def main
     app = App.new
