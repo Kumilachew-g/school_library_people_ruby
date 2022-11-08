45f00dd5a579e5b65d9392a8836d1773c5fcd50a
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-08 21:21:38 +0300

    Fix syntax errors and switch json into JSON

--- Data_Preserve/data_preserve.rb
+++ Data_Preserve/data_preserve.rb
@@ -1,36 +1,25 @@
 require './book'
 
 module DataPreserver
+  #  Add load_books method
 
-
-    #  Add load_books method
-
-    def load_books
-        file = 'books.json'
-        data = []
-        if File.exist?(file) && File.read(file) != ''
-            json.parse(File.read)(file).to_json).each do |item|
-                data.push(Book.new(item.title, item.author))
-            end
+  def load_books
+    file = 'books.json'
+    data = []
+    if File.exist?(file) && File.read(file) != ''
+      JSON.parse(File.read(file).to_json).each do |item|
+        data.push(Book.new(item.title, item.author))
+      end
     end
 
     data
-end
+  end
 
   def get_book_by_title(title)
     @books.map { |_item| item.title == title }
   end
 
-end
-         
+  # Add load_peoble method
 
-
-    # Add load_peoble method
-
-
-
-    # Add load_rentals method
-
-
-
+  # Add load_rentals method
 end
