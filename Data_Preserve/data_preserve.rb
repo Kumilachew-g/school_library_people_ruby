3b617298c35800be11e4a9521b897d1d89fe8661
Oluyemi Paul <oluola96@gmail.com>
2022-11-08 23:23:55 +0100

    Create people's file

--- Data_Preserve/data_preserve.rb
+++ Data_Preserve/data_preserve.rb
@@ -20,6 +20,27 @@ module DataPreserver
   end
 
   # Add load_peoble method
+  def load_people
+    data = []
+    file = './data/people.json'
+    if File.exist?(file) && File.read(file) != ''
+      JSON.parse(File.read(file)).each do |person|
+        if person['class'] == 'Student'
+          student = Student.new(person['age'], person['name'], person['classroom'],
+                                parent_permission: person['parent_permission'])
+          student.id = person['id']
+          data << student
+        end
+        next unless person['class'] == 'Teacher'
+
+        teacher = Teacher.new(person['age'], person['specialization'], person['name'],
+                              parent_permission: person['parent_permission'])
+        teacher.id = person['id']
+        data << teacher
+      end
+    end
+    data
+  end
 
   # Add load_rentals method
 end
