9fb49b7c83fd45261c4a91518bce4ae53165ebca
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-08 12:07:26 +0300

    Add all options in the app

--- menu.rb
+++ menu.rb
@@ -1,3 +1,16 @@
 def menu(option, app)
-   
+    case option
+    when 1
+      list_books(app)
+    when 2
+      list_person(app)
+    when 3
+      add_person(app)
+    when 4
+      add_book(app)
+    when 5
+      add_rental(app)
+    when 6
+      display_rentals_by_id(app)
+    end
   end
