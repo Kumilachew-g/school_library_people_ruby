9792e2dec5442b1cf8f894a4387a0fb47490e0fb
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-08 21:03:18 +0300

    Add load_books method functions

--- Data_Preserve/data_preserve.rb
+++ Data_Preserve/data_preserve.rb
@@ -4,8 +4,22 @@ module DataPreserver
     #  Add load_books method
 
     def load_books
-        
+        file = 'books.json'
+        data = []
+        if File.exist?(file) && File.read(file) != ''
+            json.parse(File.read)(file).to_json).each do |item|
+                data.push(Book.new(item.title, item.author))
+            end
     end
+
+    data
+end
+
+  def get_book_by_title(title)
+    @books.map { |_item| item.title == title }
+  end
+
+end
          
 
 
